8fddee0c474f3b5d638489bf8052a4cc470bf4d3
Tonmoy <shahariartonmoy17@gmail.com>
2023-11-04 21:26:55 +0600

    Sortable drag is done for large screen

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,48 +1,90 @@
 import { useEffect, useState } from "react";
 import ImageCard from "../ImageCard/ImageCard";
+import React from "react";
+import { DndContext, closestCenter,TouchSensor, closestCorners } from "@dnd-kit/core";
+import { SortableContext, arrayMove, rectSortingStrategy } from "@dnd-kit/sortable";
 
 const Home = () => {
-    const [images, setImages] = useState([]);
-    const [isSelected, setIsSelected] = useState([]);
+  const [images, setImages] = useState([]);
+  const [isSelected, setIsSelected] = useState([]);
 
-    const handleSelect = (e) =>{
-        // console.log(e.target.checked);
-        // console.log(e.target.value);
-        
-        const {value, checked} =  e.target;
-        
-        
-        checked ? setIsSelected([...isSelected,value]) : setIsSelected(isSelected.filter(id => id !== value))
-      }
-    
-    const handleDelete = ()=>{
-        setImages(images.filter(image => !isSelected.includes(image.id.toString())))
-        setIsSelected([]);
-    }
-    
-    useEffect(()=>{
-        fetch('../../../public/images.json')
-        .then(res => res.json())
-        .then(data => setImages(data))
-    },[])
+  const handleSelect = (e) => {
+    // console.log(e.target.checked);
+    // console.log(e.target.value);
 
-    return (
-        <div>
-            <div className="flex justify-between mb-5">
-                <div className="flex justify-center items-center gap-3">
-                    <input type="checkbox" checked name="" id="" />
-                    <p className="font-semibold">{isSelected.length} Files Selected</p>
-                </div>
-                <button onClick={handleDelete} className="text-red-500 font-semibold">Delete files</button>
-            </div>
-            <hr className="mb-10" />
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
-                {
-                    images.map(image => <ImageCard key={image.id} images={images} image={image}  isSelected={isSelected} handleSelect={handleSelect}></ImageCard>)
-                }
-            </div>
-        </div>
+    const { value, checked } = e.target;
+
+    checked
+      ? setIsSelected([...isSelected, value])
+      : setIsSelected(isSelected.filter((id) => id !== value));
+  };
+
+  const handleDelete = () => {
+    setImages(
+      images.filter((image) => !isSelected.includes(image.id.toString()))
     );
+    setIsSelected([]);
+  };
+
+  useEffect(() => {
+    fetch("../../../public/images.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data);
+      })
+  }, []);
+
+  //Drag
+  const [isDragging, setIsDragging] = useState(false);
+  const onDragEnd = (e) => {
+    const {active, over} = e;
+    setIsDragging(false);
+    if(active.id === over.id){
+        return;
+    }
+    setImages((images) => {
+        const oldIndex = images.findIndex((image) => image.id === active.id);
+        const newIndex = images.findIndex((image) => image.id === over.id);
+        return arrayMove(images, oldIndex, newIndex)
+    })
+  };
+  const onDragStart = () => {
+    setIsDragging(true);
+  }
+  
+
+  return (
+    <div>
+      <div className="flex justify-between mb-5">
+        <div className="flex justify-center items-center gap-3">
+          <input type="checkbox" checked name="" id="" />
+          <p className="font-semibold">{isSelected.length} Files Selected</p>
+        </div>
+        <button onClick={handleDelete} className="text-red-500 font-semibold hover:underline">
+          Delete files
+        </button>
+      </div>
+      <hr className="mb-10" />
+      <DndContext  collisionDetection={closestCorners} onDragStart={onDragStart} onDragEnd={onDragEnd}>
+        <SortableContext items={images} strategy={rectSortingStrategy}>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
+            {images.map((image, index) => (
+              <ImageCard
+                key={image.id}
+                index={index}
+                images={images}
+                image={image}
+                isDragging={isDragging}
+                isSelected={isSelected}
+                handleSelect={handleSelect}
+                
+              ></ImageCard>
+            ))}
+          </div>
+        </SortableContext>
+      </DndContext>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/ImageCard/ImageCard.jsx
+++ src/Components/ImageCard/ImageCard.jsx
@@ -1,20 +1,79 @@
-const ImageCard = ({ image, handleSelect, isSelected, images }) => {
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useEffect, useRef, useState } from "react";
 
-  const {id, imageURL} = image;
-  const colSpan = images[0].id === id ? 'lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-3' : '';
-  const inputCheckedClass = isSelected.includes(id.toString()) ? 'opacity-100' : 'bg-black opacity-0';
-  const imageCheckedClass = isSelected.includes(id.toString()) ? 'opacity-50' : 'opacity-100';
+const ImageCard = ({ image, handleSelect, isSelected, images, isDragging }) => {
+  const { id, imageURL } = image;
+  const colSpan =
+    images[0].id === id
+      ? "lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-3"
+      : "";
+  const inputCheckedClass = isSelected.includes(id.toString())
+    ? "opacity-100"
+    : "bg-black opacity-0";
+  const imageCheckedClass = isSelected.includes(id.toString())
+    ? "opacity-50"
+    : "opacity-100";
+
+  //Drag
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+
+  //handleSelect function
+  // Create a ref to capture the checkbox element
+  const checkboxRef = useRef(null);
+
+  // Use a useEffect to attach the onChange handler to the checkbox
+  useEffect(() => {
+    if (checkboxRef.current) {
+      checkboxRef.current.addEventListener("change", handleSelect);
+    }
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      if (checkboxRef.current) {
+        checkboxRef.current.removeEventListener("change", handleSelect);
+      }
+    };
+  }, [handleSelect]);
+
+  console.log(isDragging);
 
   return (
-
-      <div className={`relative border-2 rounded-2xl ${colSpan}`} draggable>
-        <img className={`rounded-2xl ${imageCheckedClass}`} src={imageURL} alt="" />
-        <div className={`absolute rounded-2xl ${inputCheckedClass} hover:opacity-50 inset-0  opacity-0  transition-opacity duration-300`}>
-          <input className='absolute z-10 left-5 top-5' type="checkbox" value={id} onChange={handleSelect} name="" id="" />
-          <div className=" absolute z-0  inset-0 "></div>
-        </div>       
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      className={`${isDragging ? '' : 'group'} relative border-2 rounded-2xl ${colSpan}`}
+      draggable
+    >
+      <img
+        className={`rounded-2xl ${imageCheckedClass}`}
+        src={imageURL}
+        alt=""
+      />
+      <div
+        className={`absolute rounded-2xl ${inputCheckedClass} group-hover:opacity-50   inset-0  opacity-0  transition-opacity duration-300`}
+      >
+        <input
+          ref={checkboxRef}
+          className="absolute z-10 left-5 top-5"
+          type="checkbox"
+          value={id}
+          onChange={handleSelect}
+          name=""
+          id=""
+        />
+        <div className=" absolute z-0  inset-0 "></div>
       </div>
-
+    </div>
   );
 };
 
